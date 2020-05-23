3e3155301e3139c45eeafb839bf2a92bc2428623
wanghg <hgwang109@fiberhome.com>
2020-05-23 16:50:36 +0800

    新增

---
+++ form-es5-1/js/myFetch.js
@@ -0,0 +1,58 @@
+
+
+var environment = 108;
+var params = {};
+var IP = "";
+var search = "";
+if(environment === 108){
+    IP = 'http://10.128.151.108:443';
+    search = '?formId=1262260388381634560&userId=10003&createDate=2020-05-21%2009:38:57'
+    search.replace(/([^?&=]+)=([^&]+)/g, function(_, k, v){params[k] = v})
+}else if(environment === 13){
+    IP = 'http://10.128.151.13:443';
+    search = '?formId=1262260388381634560&userId=10003&createDate=2020-05-21%2009:38:57'
+    search.replace(/([^?&=]+)=([^&]+)/g, function(_, k, v){params[k] = v})
+}else if(environment === 65){
+    IP = 'https://test.cictsj.com:30000';
+    search = '?formId=1262260388381634560&userId=10003&createDate=2020-05-21%2009:38:57'
+    search.replace(/([^?&=]+)=([^&]+)/g, function(_, k, v){params[k] = v})
+}else if(environment === 'all'){
+    IP = location.origin;
+    location.search.replace(/([^?&=]+)=([^&]+)/g, function(_, k, v){params[k] = v})
+}
+
+function getService(path,callback){
+    $.ajax({
+        type: 'get',
+        url: path,
+        contentType: 'application/json;charset=utf-8',
+        dataType: 'json',
+        success: function success(data) {
+            callback(data)
+        },
+        error: function error(error) {
+            callback({status:-1,errorMsg:'网络未知错误'})
+        }
+    });
+}
+
+function postService(path,body,callback){
+    $.ajax({
+        type:'get',
+        url:path,
+        contentType:'application/json;charset=utf-8',
+        dataType:'json',
+        data:JSON.stringify(body),      
+        success: function success(data) {
+            callback(data)
+        },
+        error: function error(error) {
+            callback({status:-1,errorMsg:'网络未知错误'})
+        }
+    });
+}
+
+
+
+
+
