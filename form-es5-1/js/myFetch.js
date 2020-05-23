9472deb080ffcc73f1eedbd5beabfd7d6ce2f553
wanghg <hgwang109@fiberhome.com>
2020-05-23 17:37:27 +0800

    更新

--- form-es5-1/js/myFetch.js
+++ form-es5-1/js/myFetch.js
@@ -6,7 +6,7 @@ var IP = "";
 var search = "";
 if(environment === 108){
     IP = 'http://10.128.151.108:443';
-    search = '?formId=1262260388381634560&userId=10003&createDate=2020-05-21%2009:38:57'
+    search = '?formId=1264122110670938112&userId=10003&createDate=2020-05-21%2009:38:57'
     search.replace(/([^?&=]+)=([^&]+)/g, function(_, k, v){params[k] = v})
 }else if(environment === 13){
     IP = 'http://10.128.151.13:443';
@@ -43,6 +43,23 @@ function postService(path,body,callback){
         contentType:'application/json;charset=utf-8',
         dataType:'json',
         data:JSON.stringify(body),      
+        success: function success(data) {
+            callback(data)
+        },
+        error: function error(error) {
+            callback({status:-1,errorMsg:'网络未知错误'})
+        }
+    });
+}
+
+function uploadService(path,body,callback){
+    $.ajax({
+        url:path,
+        type:"post", 
+        data:body, 
+        contentType:'application/x-www-form-urlencoded;charset=utf-8',
+        processData: false ,
+        contentType: false,
         success: function success(data) {
             callback(data)
         },
