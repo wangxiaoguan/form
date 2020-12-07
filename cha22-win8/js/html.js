e0a24d988e63fe12f0dda3bdd0acdf85b94f3325
WANGHG\wanghg <627840236@qq.com>
2020-12-07 08:59:59 +0800

    更新

--- cha22-win8/js/html.js
+++ cha22-win8/js/html.js
@@ -6,13 +6,13 @@ var htmlObject = [
         name: '基础',
         icon: '#icon-MR',
         list: [
-            { name: "字符串", address: "http://wanghg.top/html/string.html" },
-            { name: "数字", address: "http://wanghg.top/html/number.html" },
-            { name: "数组", address: "http://wanghg.top/html/array.html" },
-            { name: "JavaScript", address: "http://wanghg.top/html/javaScript.html" },
-            { name: "CSS3", address: "http://wanghg.top/html/css3.html" },
-            { name: "HTML5标签", address: "http://wanghg.top/html/element.html" },
-            { name: "时间", address: "http://wanghg.top/html/date.html" },
+            { name: "字符串", address: "http://wanghg.top/html/string.html", isShow: true },
+            { name: "数字", address: "http://wanghg.top/html/number.html", isShow: true },
+            { name: "数组", address: "http://wanghg.top/html/array.html", isShow: true },
+            { name: "JavaScript", address: "http://wanghg.top/html/javaScript.html", isShow: true },
+            { name: "CSS3", address: "http://wanghg.top/html/css3.html", isShow: true },
+            { name: "HTML5标签", address: "http://wanghg.top/html/element.html", isShow: true },
+            { name: "时间", address: "http://wanghg.top/html/date.html", isShow: true },
         ]
     },
     {
@@ -20,14 +20,14 @@ var htmlObject = [
         name: '样式',
         icon: '#icon-biaoqianA01_zhuanqu-53',
         list: [
-            { name: "服务器状态码", address: "http://wanghg.top/html/http.html" },
-            { name: "瀑布流", address: "http://wanghg.top/html/column.html" },
-            { name: "浮动", address: "http://wanghg.top/html/float.html" },
-            { name: "标签img", address: "http://wanghg.top/html/img.html" },
-            { name: "标签方法", address: "http://wanghg.top/html/string1.html" },
-            { name: "背景", address: "http://wanghg.top/html/background.html" },
-            { name: "弹性布局", address: "http://wanghg.top/html/flex.html" },
-            { name: "对象方法", address: "http://wanghg.top/html/javaScript.html" },
+            { name: "服务器状态码", address: "http://wanghg.top/html/http.html", isShow: true },
+            { name: "瀑布流", address: "http://wanghg.top/html/column.html", isShow: true },
+            { name: "浮动", address: "http://wanghg.top/html/float.html", isShow: true },
+            { name: "标签img", address: "http://wanghg.top/html/img.html", isShow: true },
+            { name: "标签方法", address: "http://wanghg.top/html/string1.html", isShow: true },
+            { name: "背景", address: "http://wanghg.top/html/background.html", isShow: true },
+            { name: "弹性布局", address: "http://wanghg.top/html/flex.html", isShow: true },
+            { name: "对象方法", address: "http://wanghg.top/html/javaScript.html", isShow: true },
         ]
     },
     {
@@ -35,9 +35,9 @@ var htmlObject = [
         name: '插件库',
         icon: '#icon-wulianwang-',
         list: [
-            { name: "Big.js运算", address: "http://wanghg.top/html/big.html" },
-            { name: "lodash.js", address: "http://wanghg.top/html/lodash.html" },
-            { name: "moment.js", address: "http://wanghg.top/html/moment.html" },
+            { name: "Big.js运算", address: "http://wanghg.top/html/big.html", isShow: true },
+            { name: "lodash.js", address: "http://wanghg.top/html/lodash.html", isShow: true },
+            { name: "moment.js", address: "http://wanghg.top/html/moment.html", isShow: true },
         ]
     },
     {
@@ -45,17 +45,20 @@ var htmlObject = [
         name: '模板',
         icon: '#icon-TEMPLATE',
         list: [
-            { name: "表单form", address: "http://wanghg.top/html/form.html" },
-            { name: "Echarts", address: "http://wanghg.top/html/echarts.html" },
-            { name: "HighCharts", address: "http://wanghg.top/html/highcharts.html" },
-            { name: "百度地图", address: "http://wanghg.top/html/map1.html" },
-            { name: "高德地图", address: "http://wanghg.top/html/map2.html" },
-            { name: "WeUI插件", address: "http://wanghg.top/html/weui/index.html" },
-            { name: "流程图", address: "http://wanghg.top/html/jsPlumb/index.html" },
-            { name: "图片特效", address: "http://wanghg.top/html/FluxSlider/index.html" },
-            { name: "图片放大", address: "http://wanghg.top/html/elevatezoom/index.html" },
-            { name: "图片操作", address: "http://wanghg.top/web/look/index.html" },
-            { name: "时间格式", address: "http://wanghg.top/html/date/index.html" },
+            { name: "表单form", address: "http://wanghg.top/html/form.html", isShow: true },
+            { name: "Echarts", address: "http://wanghg.top/html/echarts.html", isShow: true },
+            { name: "HighCharts", address: "http://wanghg.top/html/highcharts.html", isShow: true },
+            { name: "百度地图", address: "http://wanghg.top/html/map1.html", isShow: true },
+            { name: "高德地图", address: "http://wanghg.top/html/map2.html", isShow: true },
+            { name: "WeUI插件", address: "http://wanghg.top/html/weui/index.html", isShow: true },
+            { name: "流程图", address: "http://wanghg.top/html/jsPlumb/index.html", isShow: true },
+            { name: "图片特效", address: "http://wanghg.top/html/FluxSlider/index.html", isShow: true },
+            { name: "图片放大", address: "http://wanghg.top/html/elevatezoom/index.html", isShow: true },
+            { name: "图片操作", address: "http://wanghg.top/web/look/index.html", isShow: true },
+            { name: "时间格式", address: "http://wanghg.top/html/date/index.html", isShow: true },
+            { name: "轮播图", address: "http://wanghg.top/html/lunbotu.html", isShow: true },
+            { name: "分页器", address: "http://wanghg.top/html/page.html", isShow: true },
+            { name: "无缝滚动", address: "http://wanghg.top/html/move.html", isShow: true },
         ]
     },
     {
@@ -63,12 +66,12 @@ var htmlObject = [
         name: '项目',
         icon: '#icon-xiangmu',
         list: [
-            { name: "WEB1", address: "http://wanghg.top/web/web1/index.html" },
-            { name: "WEB2", address: "http://wanghg.top/web/web2/index.html" },
-            { name: "心跳", address: "http://wanghg.top/web/fund/index.html" },
-            { name: "富文本1", address: "http://wanghg.top/web/txt1/index.html" },
-            { name: "富文本2", address: "http://wanghg.top/web/txt2/index.html" },
-            { name: "购物平台", address: "http://wanghg.top/web/shopV1.0/index.html" }
+            { name: "WEB1", address: "http://wanghg.top/web/web1/index.html", isShow: true },
+            { name: "WEB2", address: "http://wanghg.top/web/web2/index.html", isShow: true },
+            { name: "心跳", address: "http://wanghg.top/web/fund/index.html", isShow: true },
+            { name: "富文本1", address: "http://wanghg.top/web/txt1/index.html", isShow: true },
+            { name: "富文本2", address: "http://wanghg.top/web/txt2/index.html", isShow: true },
+            { name: "购物平台", address: "http://wanghg.top/web/shopV1.0/index.html", isShow: true }
         ]
     },
     {
@@ -76,8 +79,8 @@ var htmlObject = [
         name: '框架',
         icon: '#icon-xiangmu',
         list: [
-            { name: "react", address: "http://wanghg.top/html/react.html" },
-            { name: "redux", address: "http://wanghg.top/html/redux.html" },
+            { name: "react", address: "http://wanghg.top/html/react.html", isShow: true },
+            { name: "redux", address: "http://wanghg.top/html/redux.html", isShow: true },
         ]
 
     },
@@ -86,11 +89,11 @@ var htmlObject = [
         name: '游戏',
         icon: '#icon-youxi',
         list: [
-            { name: "画板", address: "http://wanghg.top/html/canvas.html" },
-            { name: "切水果", address: "http://wanghg.top/html/fruit/index.html" },
-            { name: "蛇", address: "http://wanghg.top/game/she/index.html" },
-            { name: "重力骨髅", address: "http://wanghg.top/game/zhongli/index.html" },
-            { name: "坦克大战", address: "http://wanghg.top/game/war/index.html" },
+            { name: "画板", address: "http://wanghg.top/html/canvas.html", isShow: true },
+            { name: "切水果", address: "http://wanghg.top/html/fruit/index.html", isShow: true },
+            { name: "蛇", address: "http://wanghg.top/game/she/index.html", isShow: true },
+            { name: "重力骨髅", address: "http://wanghg.top/game/zhongli/index.html", isShow: true },
+            { name: "坦克大战", address: "http://wanghg.top/game/war/index.html", isShow: true },
         ]
     },
     {
@@ -98,18 +101,18 @@ var htmlObject = [
         name: '小作品',
         icon: '#icon-qita',
         list: [
-            { name: "颜色大全", address: "http://wanghg.top/html/color1.html" },
-            { name: "颜色艺名", address: "http://wanghg.top/html/color2.html" },
-            { name: "粘贴复制", address: "http://wanghg.top/html/copytxt.html" },
-            { name: "this指向", address: "http://wanghg.top/html/this.html" },
-            { name: "3D", address: "http://wanghg.top/html/3d/index.html" },
-            { name: "钟表js", address: "http://wanghg.top/zuopin/time/index.html" },
-            { name: "钟表canvas", address: "http://wanghg.top/zuopin/biao.html" },
-            { name: "艺术签名", address: "http://wanghg.top/zuopin/qianming.html" },
-            { name: "八卦图", address: "http://wanghg.top/zuopin/bagua.html" },
-            { name: "孙悟空", address: "http://wanghg.top/zuopin/monkey.html" },
-            { name: "刮奖", address: "http://wanghg.top/html/guajiang.html" },
-            { name: "自动划线", address: "http://wanghg.top/html/huaxian.html" },
+            { name: "颜色大全", address: "http://wanghg.top/html/color1.html", isShow: true },
+            { name: "颜色艺名", address: "http://wanghg.top/html/color2.html", isShow: true },
+            { name: "粘贴复制", address: "http://wanghg.top/html/copytxt.html", isShow: true },
+            { name: "this指向", address: "http://wanghg.top/html/this.html", isShow: true },
+            { name: "3D", address: "http://wanghg.top/html/3d/index.html", isShow: true },
+            { name: "钟表js", address: "http://wanghg.top/zuopin/time/index.html", isShow: true },
+            { name: "钟表canvas", address: "http://wanghg.top/zuopin/biao.html", isShow: true },
+            { name: "艺术签名", address: "http://wanghg.top/zuopin/qianming.html", isShow: true },
+            { name: "八卦图", address: "http://wanghg.top/zuopin/bagua.html", isShow: true },
+            { name: "孙悟空", address: "http://wanghg.top/zuopin/monkey.html", isShow: true },
+            { name: "刮奖", address: "http://wanghg.top/html/guajiang.html", isShow: true },
+            { name: "自动划线", address: "http://wanghg.top/html/huaxian.html", isShow: true },
         ]
     },
 ]
